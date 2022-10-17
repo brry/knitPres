4815ae427e7bf959e571b4e18f4d09283d3d90e6
brry <berryboessenkool@hotmail.com>
2022-10-17 10:29:44 +0200

    rnw2pdf now uses tinytex when available, to avoid fflush() failed (Bad file descriptor) errors

--- R/rnw2pdf.R
+++ R/rnw2pdf.R
@@ -7,6 +7,9 @@
 #' @importFrom knitr knit2pdf
 #' @export
 #' @param file  Rnw file name.
+#' @param usetinytex Logical. Use \code{tinytex::\link[tinytex]{pdflatex}}
+#'              (instead of \code{tools::\link[tools]{texi2pdf}})?
+#'              DEFAULT: TRUE when tinytex is available
 #' @param open  Logical. Should pdf be opened with
 #'              \code{berryFunctions::\link[berryFunctions]{openFile}}?
 #'              DEFAULT: TRUE
@@ -16,6 +19,7 @@
 #'
 rnw2pdf <- function(
 file,
+usetinytex=requireNamespace("tinytex", quietly=TRUE),
 open=TRUE,
 clean=TRUE,
 ...
@@ -38,8 +42,16 @@ message("Running knitr::knit on ",file," starting ",as.character(Sys.time()),"..
 texfile <- knit_with_stop(knitr::knit(file, envir=new.env(), ...)) # see internal function below
 
 # Convert to pdf:
-message("Running tools::texi2pdf on ",texfile," starting ",as.character(Sys.time()),"...")
-pdffile <- try(suppressWarnings(tools::texi2pdf(texfile, clean=FALSE)), silent=TRUE)
+if(usetinytex)
+  {
+  message("Running tinytex::pdflatex on ",texfile," starting ",as.character(Sys.time()),"...")
+  pdffile <- try(suppressWarnings(tinytex::pdflatex(texfile, clean=FALSE)), silent=TRUE)
+  } else
+  {
+  message("Running tools::texi2pdf on ",texfile," starting ",as.character(Sys.time()),"...")
+  pdffile <- try(suppressWarnings(tools::texi2pdf(texfile, clean=FALSE)), silent=TRUE)
+  }
+
 if(inherits(pdffile, "try-error"))
   open <- FALSE else
   pdffile <- normalizePath(sub("\\.Rnw$",".pdf",file), winslash="/")
